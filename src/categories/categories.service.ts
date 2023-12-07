44cf1707f5ff0b918ff2639d0734adedccdcf280
NicolasCastela <nickcastela@hotmail.com>
2023-12-07 21:30:27 +0000

    Injectabled functions CRUD, and config settings

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+
+constructor(private prisma: PrismaService){}
+
+
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({
+      data:createCategoryDto
+    })
+    }
+
+  findAll() {
+    return this.prisma.category.findMany({
+      orderBy:{
+        createdAt: 'desc',
+      }
+    });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
